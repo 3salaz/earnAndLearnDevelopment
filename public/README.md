eddc70cb7dc431d3d46f01eb99157ecb20977bc9
Erik Salazar <3salaz.dev@gmail.com>
2021-06-25 15:09:35 -0700

    added updated content

--- public/README.md
+++
@@ -1,5 +0,0 @@
-# Front End
-[Next Js](https://nextjs.org/docs/getting-started) - React
-
-[React Parallax](https://www.npmjs.com/package/react-parallax)
-
--- public/assets/scripts/videoPlayer.js
+++ public/assets/scripts/videoPlayer.js
@@ -1,134 +1,135 @@
-videoPlayer.addEventListener("loadedmetadata", function () {
-  (videoDuration = Math.round(videoPlayer.duration)),
-    videoTrack.setAttribute("max", videoDuration);
-}),
-  videoSound.addEventListener("click", function () {
-    0 == soundTrack.value
-      ? (controlVolumeStatus(1), (soundTrack.value = 10))
-      : (controlVolumeStatus(0), (soundTrack.value = 0));
-  }),
-  videoPlay.addEventListener("click", function () {
-    videoPlayer.play();
-  }),
-  videoPause.addEventListener("click", function () {
-    videoPlayer.pause();
-  }),
-  videoStart.addEventListener("click", function () {
-    videoPlayer.pause(), (videoPlayer.currentTime = 0), (videoTrack.value = 0);
-  }),
-  videoEnd.addEventListener("click", function () {
-    videoPlayer.pause(),
-      (videoPlayer.currentTime = videoDuration - 1),
-      (videoTrack.value = videoDuration - 1);
-  }),
-  videoFullScreen.addEventListener("click", function () {
-    videoPlayer.requestFullscreen
-      ? videoPlayer.requestFullscreen()
-      : videoPlayer.mozRequestFullScreen
-      ? videoPlayer.mozRequestFullScreen()
-      : videoPlayer.webkitRequestFullscreen &&
-        videoPlayer.webkitRequestFullscreen();
-  }),
-  videoTrack.addEventListener("change", function () {
-    videoPlayer.currentTime = this.value;
-  }),
-  (updateVolume = function () {
-    window.requestAnimationFrame(function () {
-      controlVolumeStatus(soundTrack.value / 10);
-    });
-  }),
-  soundTrack.addEventListener("mousedown", function () {
-    updateVolume(), soundTrack.addEventListener("mousemove", updateVolume);
-  }),
-  soundTrack.addEventListener("mouseup", function () {
-    soundTrack.removeEventListener("mousemove", updateVolume);
-  }),
-  videoPlayer.addEventListener("timeupdate", function () {
-    (videoTrack.value = videoPlayer.currentTime),
-      (videoTime.innerHTML = controlTimeFormat(videoPlayer.currentTime)),
-      Math.round(videoPlayer.currentTime) === videoDuration &&
-        setTimeout(function () {
-          (videoPlayer.currentTime = 0),
-            (videoTrack.value = 0),
-            videoPlayer.pause();
-        }, 1e3);
-  }),
-  setTimeout(function () {
-    videoPlayer.pause();
-  }, 500);
-function controlTimeFormat(e) {
-  var a = Math.floor(e / 3600),
-    t = Math.floor((e - 3600 * a) / 60),
-    o = Math.floor(e - 3600 * a - 60 * t);
-  return (
-    t < 10 && (t = "0" + t),
-    o < 10 && (o = "0" + o),
-    a < 10 && (a = "0" + a),
-    a + ":" + t + ":" + o
-  );
+var featuredVideo = document.getElementById("featuredVideo");
+var featuredVideoTitle = document.getElementById("featuredVideoTitle");
+var timeStampList = document.getElementById("timeStampList");
+var videoDescription = document.getElementById("videoDescription")
+
+var pdf = document.getElementById("pdf");
+var createList = document.createElement("LI");
+var videos = [];
+
+var tags = ["Manufaturing Week", "Guest Speakers", "Technology", "Health"];
+
+// FIREBASE CONFIGURATION
+var firebaseConfig = {
+  apiKey: "AIzaSyAr_6YLjrmuD2BrQxlO8odnOZFjeARzdBM",
+  authDomain: "earn-and-learn-6172e.firebaseapp.com",
+  databaseURL: "https://earn-and-learn-6172e-default-rtdb.firebaseio.com",
+  projectId: "earn-and-learn-6172e",
+  storageBucket: "earn-and-learn-6172e.appspot.com",
+  messagingSenderId: "629002563246",
+  appId: "1:629002563246:web:690608ce280c51492d873f",
+  measurementId: "G-DG0YFL0TB6",
+};
+// FUNCTIONS
+function loadAllVideos() {
+  $(".card").removeClass("hide");
+  pdf.classList.add("hide");
 }
-function controlVolumeStatus(e) {
-  (videoPlayer.volume = e),
-    console.log(videoPlayer.volume),
-    0 === e
-      ? videoSoundIcon.setAttribute("xlink:href", "#i-mute")
-      : videoSoundIcon.setAttribute("xlink:href", "#i-volume");
+function skipToTimeStamp(time) {
+  var MintimeStamp = time.getAttribute("data-timeStamp");
+  var timeStampArr = (MintimeStamp.split(':'));
+  let minutesInSec = (timeStampArr[0]*60);
+  var seconds = (timeStampArr[1]);
+  var timeStamp = (parseInt(minutesInSec, 10) + parseInt(seconds, 10));
+  console.log(timeStamp);
+  featuredVideo.currentTime= timeStamp;
 }
-videoPlayer.addEventListener("loadedmetadata", function () {
-  (videoDuration = Math.round(videoPlayer.duration)),
-    videoTrack.setAttribute("max", videoDuration);
-}),
-  videoSound.addEventListener("click", function () {
-    0 == soundTrack.value
-      ? (controlVolumeStatus(1), (soundTrack.value = 10))
-      : (controlVolumeStatus(0), (soundTrack.value = 0));
-  }),
-  videoPlay.addEventListener("click", function () {
-    videoPlayer.play();
-  }),
-  videoPause.addEventListener("click", function () {
-    videoPlayer.pause();
-  }),
-  videoStart.addEventListener("click", function () {
-    videoPlayer.pause(), (videoPlayer.currentTime = 0), (videoTrack.value = 0);
-  }),
-  videoEnd.addEventListener("click", function () {
-    videoPlayer.pause(),
-      (videoPlayer.currentTime = videoDuration - 1),
-      (videoTrack.value = videoDuration - 1);
-  }),
-  videoFullScreen.addEventListener("click", function () {
-    videoPlayer.requestFullscreen
-      ? videoPlayer.requestFullscreen()
-      : videoPlayer.mozRequestFullScreen
-      ? videoPlayer.mozRequestFullScreen()
-      : videoPlayer.webkitRequestFullscreen &&
-        videoPlayer.webkitRequestFullscreen();
-  }),
-  videoTrack.addEventListener("change", function () {
-    videoPlayer.currentTime = this.value;
-  }),
-  (updateVolume = function () {
-    window.requestAnimationFrame(function () {
-      controlVolumeStatus(soundTrack.value / 10);
+function skipToTime(time) {
+  featuredVideo.currentTime = time;
+}
+function loadSlide() {
+  $(".card").addClass("hide");
+  pdf.classList.remove("hide");
+}
+
+function loadVideo(videoObject) {
+  featuredVideoTitle.innerHTML = videos[videoObject][6][1];
+  featuredVideo.src = videos[videoObject][16][1];
+  var timeStampsName = videos[videoObject][0][1].split(",");
+  var timeStamps = videos[videoObject][1][1].split(",");
+  timeStampList.innerHTML = "";
+  if (timeStamps[0] === "") {
+    console.log("no timestamp 😢");
+  } else {
+    for (var i = 0; i < timeStamps.length; i++) {
+      timeStamp = timeStamps[i];
+      console.log(timeStamp);
+      timeStampList.innerHTML += `<li data-timeStamp="${timeStamp}" onclick="skipToTimeStamp(this)" id="timeStamp${i}"><button class="timeStampBtn"><h5 class="timeStampName">${timeStampsName[i]}</h5><p>${timeStamp}</p></button></li>`;
+    }
+  }
+
+  // Checking if pdf slides src is empty
+  if (videos[videoObject][12][1] === "") {
+    console.log("no slides 😢 ");
+  } else {
+    pdf.classList.remove("hide");
+    pdf.src = videos[videoObject][12][1];
+    $(".card").addClass("hide");
+    // $("#buttons").addClass("hide");
+  }
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase
+  .database()
+  .ref("videos")
+  .on("value", function (snap) {
+    snap.forEach(function (childNodes) {
+      videos.push(
+        Object.keys(childNodes.val()).map((key) => [
+          Number(key),
+          childNodes.val()[key],
+        ])
+      );
     });
-  }),
-  soundTrack.addEventListener("mousedown", function () {
-    updateVolume(), soundTrack.addEventListener("mousemove", updateVolume);
-  }),
-  soundTrack.addEventListener("mouseup", function () {
-    soundTrack.removeEventListener("mousemove", updateVolume);
-  }),
-  videoPlayer.addEventListener("timeupdate", function () {
-    (videoTrack.value = videoPlayer.currentTime),
-      (videoTime.innerHTML = controlTimeFormat(videoPlayer.currentTime)),
-      Math.round(videoPlayer.currentTime) === videoDuration &&
-        setTimeout(function () {
-          (videoPlayer.currentTime = 0),
-            (videoTrack.value = 0),
-            videoPlayer.pause();
-        }, 1e3);
-  }),
-  setTimeout(function () {
-    videoPlayer.pause();
-  }, 500);
+    // console.log(videos);
+
+    for (var i = 0; i < videos.length; i++) {
+      var video = videos[i];
+      var videoTitle = video[6][1];
+      var videoLength = video[8][1];
+      var videoTags = video[13][1];
+      var videoThumbnail = video[14][1];
+
+      $("#gallery").append(
+        `<li id="card${i}" class="card" data-tags="${videoTags}" onclick="loadVideo(${i})"><img src="${videoThumbnail}"><div class="overlay"><h5>${videoTitle}</h5><p>${videoLength}</p></div></li>`
+      );
+    }
+    
+    var $cards = $("#gallery .card");
+    var $buttons = $("#buttons");
+    var tagged = {};
+
+    $cards.each(function () {
+      var card = this;
+      var tags = $(this).data("tags");
+      if (tags) {
+        tags.split(",").forEach(function (tagName) {
+          if (tagged[tagName] == null) {
+            tagged[tagName] = [];
+          }
+          tagged[tagName].push(card);
+        });
+      }
+    });
+    $("<button />", {
+      text: "Show All",
+      class: "active",
+      click: function () {
+        $(this).addClass("active").siblings().removeClass("active");
+        $cards.show();
+      },
+    }).appendTo($buttons);
+
+    $.each(tagged, function (tagName) {
+      $("<button/>", {
+        text: tagName + " (" + tagged[tagName].length + ")",
+        click: function () {
+          $(this).addClass("active").siblings().removeClass("active");
+          $cards.hide().filter(tagged[tagName]).show();
+        },
+      }).appendTo($buttons);
+    });
+  });
